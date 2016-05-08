2b8323355a9cd83073306e2fb3d193a121ea1fe8
Adrian Matoga <epi@atari8.info>
2016-05-08 19:52:33 +0200

    Add minimal doc. Don't throw if sink just doesn't want more data.

--- source/flod/etc/curl/package.d
+++ source/flod/etc/curl/package.d
@@ -1,6 +1,13 @@
+/**
+Bindings for cURL library.
+
+Authors: $(LINK2 https://github.com/epi, Adrian Matoga)
+Copyright: © 2016 Adrian Matoga
+License: $(LINK2 https://curl.haxx.se/docs/copyright.html, MIT/X derivate license).
+*/
 module flod.etc.curl;
 
-import flod.traits;
+import flod.traits : source, Method;
 
 @source!ubyte(Method.push)
 private struct CurlReader(alias Context, A...) {
@@ -13,12 +20,10 @@ private struct CurlReader(alias Context, A...) {
 	const(char)* url;
 	Throwable e;
 
-	this()(string url)
+	this()(in char[] url)
 	{
 		this.url = url.toStringz();
 	}
-
-	pragma(msg, __traits(allMembers, typeof(this)));
 
 	private extern(C)
 	static size_t mywrite(const(void)* buf, size_t ms, size_t nm, void* obj)
@@ -43,26 +48,35 @@ private struct CurlReader(alias Context, A...) {
 		scope(exit) curl_easy_cleanup(curl);
 		if (e)
 			throw e;
+		if (err == CurlError.write_error) // all good, it's just that sink didn't want more data.
+			return;
 		enforce(err == CurlError.ok, format("libcurl: (%d) %s", err, fromStringz(curl_easy_strerror(err))));
 	}
 }
 
-auto download(string url)
+/**
+Download the resource pointed to by `url`.
+
+Example:
+----
+download("http://example.com/")
+	.byLine
+	.each!writeln;
+----
+*/
+auto download(in char[] url)
 {
-	import flod.pipeline;
-	static assert(isSchema!(typeof(pipe!CurlReader(url))));
+	import flod.pipeline : pipe;
 	return pipe!CurlReader(url);
 }
 
 unittest {
-	import std.array : appender;
-	import std.file : write, remove;
+	import std.file : write, remove, exists;
 	import std.uuid : randomUUID;
-	import flod : copy;
+	import flod : array, take;
 	auto name = "unittest-" ~ randomUUID().toString();
+	scope(exit) if (exists(name)) remove(name);
 	write(name, new ubyte[1048576]);
-	scope(exit) remove(name);
-	auto app = appender!(ubyte[]);
-	download("file://" ~ name).copy(app);
-	assert(app.data[] == new ubyte[1048576]);
+	assert(download("file://" ~ name).array() == new ubyte[1048576]);
+	assert(download("file://" ~ name).take(313377).array() == new ubyte[313377]);
 }
